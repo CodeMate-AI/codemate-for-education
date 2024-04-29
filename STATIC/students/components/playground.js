27f9b172214eea8e688909e4723c899ac8b8b71f
Kshitij Tyagi <kstyagi@scribblex.net>
2024-04-29 17:55:12 +0530

    Pushed the CODE Tag in message body inside an if statement.

--- STATIC/students/components/playground.js
+++ STATIC/students/components/playground.js
@@ -96,8 +96,10 @@ document.getElementById("send_button").onclick = ()=>{
     }
 
     let current_code = editor.getValue();
-    current_message += "\nCODE:\n";
-    current_message += current_code;
+    if(current_code !== ""){
+        current_message += "\nCODE:\n";
+        current_message += current_code;
+    }
     //current_message += "\n\nENABLED FLAGS: [--resources]"
 
     env.messages.push({
