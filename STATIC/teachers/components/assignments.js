20d512104d4d402ccc062c0362bb86e0bf34b838
mainak <heellomeelo@gmail.com>
2024-06-03 21:21:11 +0530

    created asignments page ui made and responsive

--- STATIC/teachers/components/assignments.js
+++ STATIC/teachers/components/assignments.js
@@ -1,25 +1,30 @@
 assign_teachers = {
     "pending": `     
     <div id="container-inside">
-    <div class="content">
-    <p class="title">{{assignments.pending.title}}</p>
-    <p class="description">{{assignments.pending.description}}</p>
-    <div class="date-diff date-wali-div">
-    <p class="date">Due Date : <span>{{assignments.pending.due_date}}</span></p>
-    <p class="difficulty">Difficulty : <span>{{assignments.pending.difficulty}}</span></p>
+    <img class="hidden md:block" width="50" height="50" src="https://img.icons8.com/cute-clipart/64/book.png" alt="book"/>
+    <div class="flex flex-wrap w-full justify-center sm:justify-between items-center gap-y-4">
+        <div class="content gap-y-2 md:!w-full lg:!w-[400px]">
+            <p class="title text-lg">{{assignments.pending.title}} </p>
+            <p class="description text-base text-[#7c858f]">{{assignments.pending.description}}</p>
+            <div class="flex flex-wrap gap-x-4 text-lg">
+            <div class="date flex font-semibold text-sm text-gray-900  items-center justify-center">Due Date : <span class="ml-1 text-[#69bab0]">{{assignments.pending.due_date}}</span></div>
+            <div class="difficult flex font-semibold text-sm text-gray-900  items-center justify-center">Difficulty : <span class="ml-1 text-[#69bab0]">{{assignments.pending.difficulty}}</span></div>
+            </div>
+            </div>
+            <div class="flex flex-col text-lg gap-y-4">
+                <div class="flex p-2 border-[#7c858f] border rounded-lg justify-evenly w-[280px] sm:w-[300px] items-center">
+                    <span class="text-[#4a5766] text-base">Submissions</span>
+                    <span class="font-semibold">{{assignments.pending.submissions}}</span>
+                    <span assignment_id={{assignment_id}} class="text-[#5bb7f5] bg-white font-semibold">View</span>
+                </div>
+                <div class="flex p-2 border-[#7c758f] border rounded-lg justify-evenly w-[280px] sm:w-[300px] items-center">
+                    <span class="text-[#4a5766] text-base"> Yet To Submit</span>
+                    <span class="font-semibold">{{assignments.pending.yet}}</span>
+                    <span class="text-[#5bb7f5] font-semibold">Notify</span>
+                </div>
+            </div>
     </div>
-    </div>
-    <div class="date-diff">
-    <p class="date class_____"><span class="stats___">{{assignments.pending.submissions}}</span>Submissions</p>
-    <p class="difficulty class_____"><span class="stats___">{{assignments.pending.yet}}</span><span style=""font-size: 10px;
-    font-weight: 400;
-    padding-bottom: 7px;>Yet To Submit</span></p>
-    </div>
-    <div class="bton">
-    <div class="btn-inside">
-    <button class="nav_elm" nav="submissions" assignment_id={{assignment_id}}>View Submissions</button>
-    </div>
-    </div>
+
     </div>
     `
     };
