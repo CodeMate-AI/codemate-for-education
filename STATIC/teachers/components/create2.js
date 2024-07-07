c80e3ff37f9014b0211e2731ab80cf4bfa29c6ac
mainak <heellomeelo@gmail.com>
2024-07-07 21:01:30 +0530

    continue functionality and some UI issues need to be fixed

--- STATIC/teachers/components/create2.js
+++ STATIC/teachers/components/create2.js
@@ -1,5 +1,5 @@
 //stepper logic
-var currentTab = 1; // Current tab is set to be the first tab (0)
+var currentTab = 0; // Current tab is set to be the first tab (0)
         showTab(currentTab); // Display the current tab
         
         function showTab(n) {
@@ -98,29 +98,32 @@ setInterval(checkFields, 1000);
 
 //now "do magic" logic
 
-let easyProblem = { title: "sdsdsdsd", description: "sdsdsdsd", codingProblem: "sdsdsd" };
-let mediumProblem = { title: "sdsd", description: "sdsdsd", codingProblem: "sdsdsd" };
-let hardProblem = { title: "sdsdsd", description: "sdsdsd", codingProblem: "sdsdsd" };
+let easyProblem = { title: "sdsdsdsd", description: "sdsdsdsd", codingProblem: "sdsdsd", selected:false };
+let mediumProblem = { title: "sdsd", description: "sdsdsd", codingProblem: "sdsdsd", selected:false };
+let hardProblem = { title: "sdsdsd", description: "sdsdsd", codingProblem: "sdsdsd", selected:false };
 
 //helper function to extract values
 function extractProblemDetails(response) {
-  const titleMatch = response.match(/Title:\s*(.+?)\n\n/);
-  const descriptionMatch = response.match(/Description:\s*(.+?)\n\n/);
-  const codingProblemMatch = response.match(/CodingProblem:\s*(.+?)\n\n/);
+  const titleMatch = response.match(/Title:\s*(.*?)\.\s*Description:/);
+  const descriptionMatch = response.match(/Description:\s*(.*?)\.\s*CodingProblem:/);
+  const codingProblemMatch = response.match(/CodingProblem:\s*(.*?)\s*Resources:/);
 
-  const title = titleMatch ? titleMatch[1] : null;
-  const description = descriptionMatch ? descriptionMatch[1] : null;
-  const codingProblem = codingProblemMatch ? codingProblemMatch[1] : null;
-  return {
-      title: titleMatch ? titleMatch[1] : null,
-      description: descriptionMatch ? descriptionMatch[1].trim() : null,
-      codingProblem: codingProblemMatch ? codingProblemMatch[1].trim() : null
-  };
+  const title = titleMatch ? titleMatch[1] : '';
+  const description = descriptionMatch ? descriptionMatch[1] : '';
+  const codingProblem = codingProblemMatch ? codingProblemMatch[1] : '';
+
+  return { title, description, codingProblem };
 }
 
 // function to handle individual fetch requests
 
 async function fetchTask(content, element, difficulty) {
+  if (difficulty == "easy")
+    document.getElementById("loader-text").innerText = "Creating Easy Problem...";
+  if (difficulty == "medium")
+    document.getElementById("loader-text").innerText = "Creating Medium Problem...";
+  if (difficulty === "hard")
+    document.getElementById("loader-text").innerText = "Creating Hard Problem...";
   try {
     const response = await fetch("http://localhost:8000/generate", {
       method: "POST",
@@ -174,9 +177,9 @@ async function doMagic(event) {
 
   //helps in running things parallely
   const tasks = [
-    await fetchTask(`QUERY:\nGenerate a problem statement of easy nature using this title: ${titleValue}, description: ${descriptionValue}, and coding problem: ${codingProblemValue} .Give the output in this format:"Title:(improved title). Description: (improved description). CodingProblem: (only problem) .Resources:(only one youtube link)"`, easyStatementElement,"easy"),
-    await fetchTask(`QUERY:\nGenerate a problem statement of medium nature using this title: ${titleValue}, description: ${descriptionValue}, and coding problem: ${codingProblemValue} .Give the output in this format:"Title:(improved title). Description: (improved description). CodingProblem: (only problem) .Resources:(only one youtube link)"`, mediumStatementElement,"medium"),
-    await fetchTask(`QUERY:\nGenerate a problem statement of hard nature using this title: ${titleValue}, description: ${descriptionValue}, and coding problem: ${codingProblemValue} .Give the output in this format:"Title:(improved title). Description: (improved description). CodingProblem: (only problem) .Resources:(only one youtube link)"`, hardStatementElement,"hard")
+    await fetchTask(`Generate a problem statement of easy nature using this title: ${titleValue}, description: ${descriptionValue}, and coding problem: ${codingProblemValue} .Give the output in this format:"Title:(improved title). Description: (improved description). CodingProblem: (only problem) .Resources:(only one youtube link)." Make sure the response string has got no newline character (\n). Maintain exactly this format: response string is 'Title:....(space).(space)Description:....(space).(space)CodingProblem:....(space).(space)REsources:....'`, easyStatementElement,"easy"),
+    await fetchTask(`Generate a problem statement of medium nature using this title: ${titleValue}, description: ${descriptionValue}, and coding problem: ${codingProblemValue} .Give the output in this format:"Title:(improved title). Description: (improved description). CodingProblem: (only problem) .Resources:(only one youtube link)." Make sure the response string has got no newline character (\n). Maintain exactly this format: response string is 'Title:....(space).(space)Description:....(space).(space)CodingProblem:....(space).(space)REsources:....'`, mediumStatementElement,"medium"),
+    await fetchTask(`Generate a problem statement of hard nature using this title: ${titleValue}, description: ${descriptionValue}, and coding problem: ${codingProblemValue} .Give the output in this format:"Title:(improved title). Description: (improved description). CodingProblem: (only problem) .Resources:(only one youtube link)." Make sure the response string has got no newline character (\n). Maintain exactly this format: response string is 'Title:....(space).(space)Description:....(space).(space)CodingProblem:....(space).(space)REsources:....'`, hardStatementElement,"hard")
   ];
 
   //the below portion was how the plan was supposed to be but it failed since the same server is being used and it cant
@@ -221,3 +224,148 @@ function closeProblemModal() {
   document.getElementById("mediumView").style.display = "none";
   document.getElementById("hardView").style.display = "none";
 }
+
+
+//edit and save logic
+function editEasyProblem() {
+  document.getElementById("easyViewTitle").style.display = "none";
+  document.getElementById("easyViewDescription").style.display = "none";
+  document.getElementById("easyViewCodingProblem").style.display = "none";
+  document.getElementById("easyEditTitle").style.display = "flex";
+  document.getElementById("easyEditDescription").style.display = "flex";
+  document.getElementById("easyEditCodingProblem").style.display = "flex";
+  document.getElementById("easyEditTitle").focus();
+  document.getElementById("easyEditTitle").value = easyProblem.title;
+  document.getElementById("easyEditDescription").value = easyProblem.description;
+  document.getElementById("easyEditCodingProblem").value = easyProblem.codingProblem;
+}
+
+function saveEasyProblem() {
+  easyProblem.title=document.getElementById("easyEditTitle").value;
+  easyProblem.description=document.getElementById("easyEditDescription").value;
+  easyProblem.codingProblem = document.getElementById("easyEditCodingProblem").value;
+  document.getElementById("easyEditTitle").style.display = "none";
+  document.getElementById("easyEditDescription").style.display = "none";
+  document.getElementById("easyEditCodingProblem").style.display = "none";
+  document.getElementById("easyViewTitle").style.display = "flex";
+  document.getElementById("easyViewDescription").style.display = "flex";
+  document.getElementById("easyViewCodingProblem").style.display = "flex";
+  document.getElementById("easyViewTitle").innerText = easyProblem.title;
+  document.getElementById("easyViewDescription").innerText = easyProblem.description;
+  document.getElementById("easyViewCodingProblem").innerText = easyProblem.codingProblem;
+  document.getElementById('easyStatement').innerText = easyProblem.codingProblem;
+  var notyf = new Notyf();
+  notyf.success("Changes made successfully!")
+}
+
+function editMediumProblem() {
+  document.getElementById("mediumViewTitle").style.display = "none";
+  document.getElementById("mediumViewDescription").style.display = "none";
+  document.getElementById("mediumViewCodingProblem").style.display = "none";
+  document.getElementById("mediumEditTitle").style.display = "flex";
+  document.getElementById("mediumEditDescription").style.display = "flex";
+  document.getElementById("mediumEditCodingProblem").style.display = "flex";
+  document.getElementById("mediumEditTitle").focus();
+  document.getElementById("mediumEditTitle").value = mediumProblem.title;
+  document.getElementById("mediumEditDescription").value = mediumProblem.description;
+  document.getElementById("mediumEditCodingProblem").value = mediumProblem.codingProblem;
+}
+
+function saveMediumProblem() {
+  mediumProblem.title=document.getElementById("mediumEditTitle").value;
+  mediumProblem.description=document.getElementById("mediumEditDescription").value;
+  mediumProblem.codingProblem = document.getElementById("mediumEditCodingProblem").value;
+  document.getElementById("mediumEditTitle").style.display = "none";
+  document.getElementById("mediumEditDescription").style.display = "none";
+  document.getElementById("mediumEditCodingProblem").style.display = "none";
+  document.getElementById("mediumViewTitle").style.display = "flex";
+  document.getElementById("mediumViewDescription").style.display = "flex";
+  document.getElementById("mediumViewCodingProblem").style.display = "flex";
+  document.getElementById("mediumViewTitle").innerText = mediumProblem.title;
+  document.getElementById("mediumViewDescription").innerText = mediumProblem.description;
+  document.getElementById("mediumViewCodingProblem").innerText = mediumProblem.codingProblem;
+  document.getElementById('mediumStatement').innerText = mediumProblem.codingProblem;
+  var notyf = new Notyf();
+  notyf.success("Changes made successfully!")
+}
+
+function editHardProblem() {
+  document.getElementById("hardViewTitle").style.display = "none";
+  document.getElementById("hardViewDescription").style.display = "none";
+  document.getElementById("hardViewCodingProblem").style.display = "none";
+  document.getElementById("hardEditTitle").style.display = "flex";
+  document.getElementById("hardEditDescription").style.display = "flex";
+  document.getElementById("hardEditCodingProblem").style.display = "flex";
+  document.getElementById("hardEditTitle").focus();
+  document.getElementById("hardEditTitle").value = hardProblem.title;
+  document.getElementById("hardEditDescription").value = hardProblem.description;
+  document.getElementById("hardEditCodingProblem").value = hardProblem.codingProblem;
+}
+
+function saveHardProblem() {
+  hardProblem.title=document.getElementById("hardEditTitle").value;
+  hardProblem.description=document.getElementById("hardEditDescription").value;
+  hardProblem.codingProblem = document.getElementById("hardEditCodingProblem").value;
+  console.log(`document.getElementById("hardEditTitle").value=`, document.getElementById("hardEditTitle").value)
+  console.log('hard problem=',hardProblem)
+  document.getElementById("hardEditTitle").style.display = "none";
+  document.getElementById("hardEditDescription").style.display = "none";
+  document.getElementById("hardEditCodingProblem").style.display = "none";
+  document.getElementById("hardViewTitle").style.display = "flex";
+  document.getElementById("hardViewDescription").style.display = "flex";
+  document.getElementById("hardViewCodingProblem").style.display = "flex";
+  document.getElementById("hardViewTitle").innerText = hardProblem.title;
+  document.getElementById("hardViewDescription").innerText = hardProblem.description;
+  document.getElementById("hardViewCodingProblem").innerText = hardProblem.codingProblem;
+  document.getElementById('hardStatement').innerText = hardProblem.codingProblem;
+  var notyf = new Notyf();
+  notyf.success("Changes made successfully!")
+}
+
+//select problem logic
+function selectEasy() {
+  document.getElementById("easyUnselected").style.display = "none";
+  document.getElementById("easySelected").style.display = "block";
+  easyProblem.selected = true;
+  mediumProblem.selected = false;
+  hardProblem.selected = false;
+  document.getElementById("mediumUnselected").style.display = "block";
+  document.getElementById("mediumSelected").style.display = "none";
+  document.getElementById("hardUnselected").style.display = "block";
+  document.getElementById("hardSelected").style.display = "none";
+}
+
+function selectMedium() {
+  document.getElementById("mediumUnselected").style.display = "none";
+  document.getElementById("mediumSelected").style.display = "block";
+  easyProblem.selected = false;
+  mediumProblem.selected = true;
+  hardProblem.selected = false;
+  document.getElementById("easyUnselected").style.display = "block";
+  document.getElementById("easySelected").style.display = "none";
+  document.getElementById("hardUnselected").style.display = "block";
+  document.getElementById("hardSelected").style.display = "none";
+}
+
+function selectHard() {
+  document.getElementById("hardUnselected").style.display = "none";
+  document.getElementById("hardSelected").style.display = "block";
+  easyProblem.selected = false;
+  mediumProblem.selected = false;
+  hardProblem.selected = true;
+  document.getElementById("easyUnselected").style.display = "block";
+  document.getElementById("easySelected").style.display = "none";
+  document.getElementById("mediumUnselected").style.display = "block";
+  document.getElementById("mediumSelected").style.display = "none";
+}
+
+//activate continue if anyone problem is selected , keep checking every 500ms
+
+function checkSelect() {
+  if (easyProblem.selected || mediumProblem.selected || hardProblem.selected)
+    document.getElementById("continue").disabled = false;
+  else
+    document.getElementById("continue").disabled = true;
+}
+
+setInterval(checkSelect, 500);
